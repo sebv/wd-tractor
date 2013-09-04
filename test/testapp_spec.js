c11d941f19df24fa2644a53dbdbd3af8f2ebb5fd
sebv <seb.vincent@gmail.com>
2013-09-04 12:24:09 +0800

    made element wrapping work

--- test/testapp_spec.js
+++ test/testapp_spec.js
@@ -1,4 +1,4 @@
-/* global describe, it, before, beforeEach */
+/* global describe, it, before, after, beforeEach */
 
 var wd = require('../lib/main');
 //var util = require('util');
@@ -32,6 +32,10 @@ describe('longer example', function() {
     ], done);
   });
 
+  after(function(done) {
+    browser.quit(done);
+  });
+
   describe('synchronizing with Angular', function() {
     beforeEach(function(done) {
       browser.get(urlRoot + 'app/index.html', done);
@@ -44,11 +48,11 @@ describe('longer example', function() {
         function(done) {browser.elementById('fetch', done);}
       ], function(err, res) {
         if(err) {return done(err);}
-        var sample1Button = res[0];
+        //var sample1Button = res[0];
         var sample2Button = res[1];
         var fetchButton = res[2];
         async.series([
-          function(done) { browser.clickElement(fetchButton, done); },
+          function(done) { fetchButton.click(done); },
           // The quick RPC works fine.
 
           function(done) {browser.elementByNgBinding('{{status}}', function(err, elem) {
@@ -58,7 +62,7 @@ describe('longer example', function() {
             elemTextShouldEqual(browser, elem, 'done', done);
           });},
           function(done) {browser.clickElement(sample2Button, done);},
-          function(done) {browser.clickElement(fetchButton, done);},
+          function(done) {fetchButton.click(done);},
           function(done) {browser.elementById('statuscode', function(err, elem) {
             elemTextShouldEqual(browser, elem, '200', done);
           });},
@@ -70,41 +74,31 @@ describe('longer example', function() {
       }, done);
     });
 
-    // describe('slow rendering', function() {
-    //   beforeEach(function(done) {
-    //     browser.get(urlRoot + 'app/index.html#/repeater', done);
-    //   });
+    describe('slow rendering', function() {
+      beforeEach(function(done) {
+        browser.get(urlRoot + 'app/index.html#/repeater', done);
+      });
 
-    //   it('should synchronize with a slow action', function(done) {
-    //     var addOneButton;
-    //     async.waterfall([
-    //       function(done) {browser.elementById('addone', done);},
-    //       function(_addOneButton, done) {
-    //         addOneButton = _addOneButton;
-    //         browser.click(addOneButton, done);
-    //       },
-    //     ]);
-    //     browser.elementById('addone', function(err, addOneButton) {
-    //       if(err) {return done(err);}
-    //       async.series([
-    //         function(done) {browser.click(addOneButton, done);},
-    //       ], done);
-    //     });
-    //     // var topNumber = ptor.findElement(
-    //     //     protractor.By.repeater('foo in foos').row(1).
-    //     //     column('{{foo.b}}'));
-
-    //     // expect(topNumber.getText()).toEqual('14930352');
-
-    //     // addOneButton.click();
-
-    //     // topNumber = ptor.findElement(
-    //     //     protractor.By.repeater('foo in foos').row(1).
-    //     //     column('{{foo.b}}'));
-
-    //     // expect(topNumber.getText()).toEqual('24157817');
-    //   });
-    // });
+      it('should synchronize with a slow action', function(done) {
+        var addOneButton;
+        async.waterfall([
+          function(done) {browser.elementById('addone', done);},
+          function(_addOneButton, done) {
+            addOneButton = _addOneButton;
+            browser.clickElement(addOneButton, done);
+          },
+          function(done) {browser.elementByNgRepeater('foo in foos', 1, '{{foo.b}}', done);},
+          function(topNumberElem , done) {
+            elemTextShouldEqual(browser, topNumberElem, '14930352', done);
+          },
+          function(done) {browser.clickElement(addOneButton, done);},
+          function(done) {browser.elementByNgRepeater('foo in foos', 1, '{{foo.b}}', done);},
+          function(topNumberElem , done) {
+            elemTextShouldEqual(browser, topNumberElem, '24157817', done);
+          }
+        ], done);
+      });
+    });
 
 
   });
